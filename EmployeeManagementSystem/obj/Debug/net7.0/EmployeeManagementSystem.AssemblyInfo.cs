1b5eae9708c68bd123544f39db93d75519c99a5c
viyan.anbazhagan <viyan.anbazhagan@aspiresys.com>
2024-01-17 11:33:16 +0530

    First Commit

--- EmployeeManagementSystem/obj/Debug/net7.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ EmployeeManagementSystem/obj/Debug/net7.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c43149b601b9ec783fe119877ba44a936603587")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
